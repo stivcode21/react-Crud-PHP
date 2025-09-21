9eb882a35d1cef2db2e47b7dbc776b94f781f603
stivcode21 <stivenelcodificador@gmail.com>
2025-09-20 21:26:56 -0500

    feat: createUser now capture all data of the inputs

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+
 const CreateUser = () => {
-  return <h1>CreateUser</h1>;
+  const [inputs, setInputs] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(inputs);
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  return (
+    <div>
+      <h1>CreateUser</h1>
+      <form onSubmit={handleSubmit}>
+        <table cellSpacing="10">
+          <tbody>
+            <tr>
+              <th>
+                <label htmlFor="name">Name:</label>
+              </th>
+              <td>
+                <input type="text" name="name" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <label htmlFor="email">Email:</label>
+              </th>
+              <td>
+                <input type="email" name="email" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <label htmlFor="mobile" name="mobile">
+                  Mobile:
+                </label>
+              </th>
+              <td>
+                <input type="text" name="mobile" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan="2" align="rigth">
+                <button>Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
 };
 
 export default CreateUser;
